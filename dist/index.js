7f6fc7f6de8292654bfc5c295ba7142ee4bb804d
Raghav Tandon <raghavtan@gmail.com>
2022-03-17 11:23:33 +0100

    complete compilation with set status

--- dist/index.js
+++ dist/index.js
@@ -8482,6 +8482,7 @@ const main = async () => {
       const run_id = core.getInput('TARGET_RUN_ID', { required: true });
       const output_env = core.getInput('OUTPUT_ENV_NAME', { required: true });
       const token = core.getInput('GITHUB_TOKEN', { required: true });
+      const ignore_regex = core.getInput('IGNORE_JOBS', { required: true });
   
       /**
        * Now we need to create an instance of Octokit which will use to call
@@ -8509,10 +8510,36 @@ const main = async () => {
       /**
        * Loop over all the jobs that were executed in the run and store them in a variable.
        **/
+      var integral_jobs = []
+      var all_jobs = []
        for (const job of jobs_list.jobs) {
-           console.log(`${job.name}, ${job.status}, ${job.conclusion}`);
-           core.info(`${job.name}, ${job.status}, ${job.conclusion}`);
+        /** 
+         * copy status, name, and conclusion to a new object
+         * 
+         *  
+         * check if the job.name matches the regex provided by the user(ignore_regex)
+         * if it does not match, then we will add it to the integral_jobs array
+         * if it does match, then we will skip it
+          **/
+        all_jobs.push({name: job.name, status: job.status, conclusion: job.conclusion})
+
+        if(!job.name.match(ignore_regex)){
+          integral_jobs.push({name: job.name, status: job.status, conclusion: job.conclusion})
+        }
+           
        }
+       /**
+        * check if the conclusion of all the jobs is success
+        * if it is not success, then we set status to failure
+        * else we set status to success
+        * 
+        **/
+        var status = 'success'
+        for (const job of integral_jobs) {
+          if(job.conclusion != 'success'){
+            status = 'failure'
+          }
+        }
 
         /**
          * Now we export the variable to the environment.
@@ -8520,23 +8547,41 @@ const main = async () => {
          * Reference: https://octokit.github.io/rest.js/v18#actions-create-status
         **/
 
+         core.exportVariable(output_env, status);
+         core.setOutput('status', status);
+
         /**
          * Output the variables to the console.
          * This will be displayed in the GitHub Actions UI.
          * 
          * Reference: https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions
         **/
-        core.startGroup('Job Details');
-        core.notice(jobs_list.job);
-        core.endGroup('Job Details');
+        core.startGroup('All Job Details');
+        core.notice(JSON.stringify(all_jobs, null, 2));
+        core.endGroup('All Job Details');
 
-        core.exportVariable(output_env, status);
-        core.setOutput('status', status);
+        core.startGroup('Integral Job Details');
+        core.notice(JSON.stringify(integral_jobs, null, 2));
+        core.endGroup('Integral Job Details');
 
-        /** create check_run with status
+        /** create check_run with status variable for this pull_request
          * 
          * Reference: https://octokit.github.io/rest.js/v18#checks-create-check-run
-         */ 
+        **/ 
+        await octokit.checks.create({
+          owner,
+          repo,
+          name: 'Status Check',
+          head_sha: github.context.sha,
+          status: status,
+          started_at: new Date().toISOString(),
+          completed_at: new Date().toISOString(),
+          output: {
+            title: 'Job Status Compilation',
+            summary: 'Integration',
+            text: JSON.stringify(all_jobs, null, 2),
+          }
+        });
         
 
     } catch (error) {
