d1aeccd9d6909de865ad7ffe1dc7d17f8171e141
Raghav Tandon <raghavtan@gmail.com>
2022-03-21 12:58:23 +0100

    fix eslint issues

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+/* eslint-disable max-len */
 const core = require('@actions/core');
 const github = require('@actions/github');
-import tablemark from "tablemark"
+import tablemark from 'tablemark';
 
 const main = async () => {
   const owner = github.context.repo.owner;
@@ -10,10 +11,11 @@ const main = async () => {
   const ignoreJobsRegex = core.getInput('ignore-jobs');
   const checkRunTitle = core.getInput('check-run-title');
   const checkRunName = core.getInput('check-run-name');
-  
+
   console.log(github.context);
 
   // see: https://octokit.github.io/rest.js/v18
+  // eslint-disable-next-line new-cap
   const octokit = new github.getOctokit(token);
 
   // see: https://octokit.github.io/rest.js/v18#actions-list-jobs-for-workflow-run
@@ -23,8 +25,8 @@ const main = async () => {
     runId,
   });
 
-  const filteredJobs = response.data.jobs.filter(job => !ignoreRegex || !job.name.match(ignoreJobsRegex));
-  const failure = filteredJobs.some(job => job.conclusion == 'failure');
+  const filteredJobs = response.data.jobs.filter((job) => !ignoreRegex || !job.name.match(ignoreJobsRegex));
+  const failure = filteredJobs.some((job) => job.conclusion == 'failure');
 
   // see: https://octokit.github.io/rest.js/v18#checks-create-check-run
   await octokit.rest.checks.create({
@@ -32,14 +34,14 @@ const main = async () => {
     repo,
     name: checkRunName,
     head_sha: github.context.payload.pull_request.head.sha,
-    status: "completed",
-    conclusion: failure ? "failure" : "success",
+    status: 'completed',
+    conclusion: failure ? 'failure' : 'success',
     output: {
       title: checkRunTitle,
       summary: tablemark(filteredJobs),
       text: tablemark(filteredJobs),
-    }
+    },
   });
-}
+};
 
 main();
